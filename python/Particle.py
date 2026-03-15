e207b057ef91e42d5ca99afdbd273858a93b73da
Matías Soto Lomboy <mati.sotitol@gmail.com>
2026-03-15 17:43:08 -0300

    versión inicial pso python

--- python/Particle.py
+++ python/Particle.py
@@ -1,41 +1,76 @@
 import pygame
 import random
-
+import math
 
 class Particle:
-    def __init__(self, x, y, radius = 8, color = (170, 0, 255)):
-        self.pos = [x, y]
-        self.vel = [random.randint(2, 5), random.randint(2, 5)]
-        self.radius = radius
+    def __init__(self, radius = 8, color = (170, 0, 255)):
         self.color = color
-        self.pbest = [0, 0]
+        self.radius = radius
+
+        # Posición
+        self.x = random.uniform(0, radius * 2)
+        self.y = random.uniform(0, radius * 2)
+
+        # Velocidad
+        self.vx = random.uniform(-1, 1)
+        self.vy = random.uniform(-1, 1)
+
+        # Posición del personal best
+        self.px = self.x
+        self.py = self.y
+
+        # Personal best
+        self.pbest = -999999
+
 
     def draw(self, screen):
         pygame.draw.circle(
             screen,
             self.color,
-            self.pos,
+            (int(self.x), int(self.y)),
             self.radius,
             width = 2
         )
 
-    def update_pos(self, width, height):
-        # Actualizar posición según velocidad
-        self.pos = [self.pos[0] + self.vel[0], self.pos[1] + self.vel[1]]
 
-        # Rebotes con las paredes
-        if self.pos[0] - self.radius < 0:
-            self.pos[0] = self.radius
-            self.vel[0] *= -1
+    def evaluate(self, heatmap: pygame.Surface):
+        # Obtener los colores de la imagen (mapa de calor)
+        r, g, b, _ = heatmap.get_at((int(self.x), int(self.y)))
 
-        elif self.pos[0] + self.radius > width:
-            self.pos[0] = width - self.radius
-            self.vel[0] *= -1
+        # b - r -> Buscar las zonas más azules
+        fitness = b - r
 
-        if self.pos[1] - self.radius < 0:
-            self.pos[1] = self.radius
-            self.vel[1] *= -1
+        if fitness > self.pbest:
+            self.pbest = fitness
+            self.px = self.x
+            self.py = self.y
 
-        elif self.pos[1] + self.radius > height:
-            self.pos[1] = height - self.radius
-            self.vel[1] *= -1
+        return fitness
+
+
+    def move(self, screen, gbest, w = 0.7, c1 = 1.4, c2 = 1.4, max_vel = 4):
+        # gbest = Mejor posición vista por cualquier partícula
+        # w = Inercia
+        # c1 = Factor de aprendizaje individual
+        # c2 = Factor de aprendizaje según el éxito global 
+        # max_vel = Velocidad máxima que puede tomar la partícula
+
+        px = py = random.random()
+
+        # Actualizar la velocidad
+        self.vx = w * self.vx + c1 * px * (self.px - self.x) + c2 * px * (gbest[0] - self.x)
+        self.vy = w * self.vy + c1 * py * (self.py - self.y) + c2 * py * (gbest[0] - self.y)
+        
+        # Regular la velocidad de la partícula
+        speed = math.sqrt(self.vx ** 2 + self.vy ** 2)
+        if speed > max_vel:
+            self.vx = self.vx / speed * max_vel
+            self.vy = self.vy / speed * max_vel
+
+        # Actualizar la posición de la partícula
+        self.x += self.vx
+        self.y += self.vy
+
+        # Manejo de bordes
+        self.x = max(0, min(screen.width - 1, self.x))
+        self.y = max(0, min(screen.height - 1, self.y))
--- python/pso.py
+++ python/pso.py
@@ -4,7 +4,6 @@ import random
 from Particle import Particle
 
 VELOCITY = 3
-W = 200
 
 class Simulacion:
     def __init__(self):
@@ -18,14 +17,16 @@ class Simulacion:
         self.bg = pygame.transform.scale(pygame.image.load("assets/heatmap.png").convert(), (self.width, self.height + 100))
 
         self.particles = [
-            Particle(random.randint(0, self.width), random.randint(0, self.height)) for _ in range(50)
+            Particle() for _ in range(50)
         ]
 
-        self.gbest = [0, 0]
+        self.gbest = (0, 0)
+        self.gbest_fitness = -999999
 
         self.clock = pygame.time.Clock()
         self.running = True
 
+    # Main loop
     def run(self):
         while self.running:
             self.handle_events()
@@ -35,17 +36,33 @@ class Simulacion:
 
         pygame.quit()
 
+
+    # Procesar los eventos del usuario
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
 
-    def update(self):
-        for particle in self.particles:
-            particle.update_pos(self.width, self.height)
 
+    # Actualizar lógica
+    def update(self):
+
+        # Evaluar el fitness
+        for particle in self.particles:
+            fitness = particle.evaluate(self.bg)
+
+            if fitness > self.gbest_fitness:
+                self.gbest_fitness = fitness
+                self.gbest = (particle.x, particle.y)
+
+        # Mover las partículas
+        for particle in self.particles:
+            particle.move(self.screen, self.gbest)
+
+
+    # Renderizar assets y objetos
     def render(self):
-        self.screen.blit(self.bg, (0, 0))  # color de fondo
+        self.screen.blit(self.bg, (0, 0))  # imagen de fondo
 
         for particle in self.particles:
             particle.draw(self.screen)
