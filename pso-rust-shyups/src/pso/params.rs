8624b17cb60b0256c0cc32218a30871eb074d92c
Diego Soto <diegosotovergara17@gmail.com>
2026-04-04 21:17:18 -0300

    crazy change

--- pso-rust-shyups/src/pso/params.rs
+++ pso-rust-shyups/src/pso/params.rs
@@ -5,11 +5,11 @@ pub const Y_MIN: f32 = -3.0;
 pub const Y_MAX: f32 = 7.0;
  
 // === PARÁMETROS PSO ===
-pub const N_PARTICULAS: usize = 3000;
-pub const V_MAX: f32 = 0.03;
-pub const W: f32 = 3.0;   // inercia
-pub const C1: f32 = 20.0;  // componente cognitivo
-pub const C2: f32 = 50.0;  // componente social
+pub const N_PARTICULAS: usize = 1000;
+pub const V_MAX: f32 = 0.1;
+pub const W: f32 = 1000.0;   // inercia
+pub const C1: f32 = 50.0;  // componente cognitivo
+pub const C2: f32 = 20.0;  // componente social
 pub const RUIDO: f32 = 0.0;
  
 // === VISUALIZACIÓN ===
